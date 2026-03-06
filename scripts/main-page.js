2d17745c648cc0cd73a24f531d06cbc460778ef2
greed <rakib123kib.ra@gmail.com>
2026-03-07 04:23:04 +0600

    added button section

---
+++ scripts/main-page.js
@@ -0,0 +1,15 @@
+const allBtn = document.getElementById("all-btn");
+const openBtn = document.getElementById("open-btn");
+const closedBtn = document.getElementById("closed-btn");
+
+const setActive = (id) => {
+  btns = [allBtn, openBtn, closedBtn];
+  for (btn of btns) {
+    btn.classList.remove("btn-primary");
+    if (id === btn) {
+      id.classList.add("btn-primary");
+    }
+  }
+};
+
+setActive();
